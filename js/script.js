f97a3af3dea2caf7a94712633e02cf8fdb5b3e01
Anthony Iamurri <anthony.iamurri@gmail.com>
2017-11-01 09:11:42 -0400

    jQuery lab 2 unfinished

--- js/script.js
+++ js/script.js
@@ -1,22 +1,52 @@
 $(document).ready(function() {
 
-  $(function() {
-    $(".available").click(function() {
-      $('#form').show();
+  var tableInfo = [null, null, null, null, null, null, null, null, null];
+
+  var currentTable = 0;
+
+  var tablenum = 0;
+
+  var lastTableClicked = 0;
+
+  // show reservation pop-up when available table clicked
+
+  $(".available").on("click", function(event) {
+    var tablenum = $(this).attr("data-tablenum");
+    tablenum = parseInt(tablenum);
+    tablenum = Number(tablenum);
+    lastTableClicked = this;
+    $("#tableNumber").text("Table Number: " + tablenum);
+    $("#form").fadeIn();
+    $("#reserve").click(function() {
+      $('#form').fadeOut();
+      // $(this).toggleClass("reserved");
     });
+
   });
+
+
+  var tableNumber = $("");
+
+  {
+    $("#tables").attr("data-tablenum");
+  }
+
+
+  // exit reservation pop-up
 
   $(function() {
     $("#close").click(function() {
-      $('#form').hide();
+      $('#form').fadeOut();
     });
   });
 
-  $(function() {
-    $("#reserve").click(function() {
-      $('#form').hide();
-      $(".available").addClass("reserved");
-    });
-  });
+  // reserve table
+
+  // $(function() {
+  //   $("#reserve").click(function() {
+  //     $('#form').hide();
+  //     $(tablenum).toggleClass("reserved");
+  //   });
+  // });
 
 });
